dd345e62cdcace043b93dbe6bef269fca163ddbd
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2024-11-19 01:51:16 -0300

    chore: configurar servidor inicial

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import userRoutes from './routes/userRoutes';
+import taskRoutes from './routes/taskRoutes';
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
+
+export default app;
