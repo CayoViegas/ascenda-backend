bd77fe6454d25d56306e451f854c1da442053266
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2024-11-20 02:32:38 -0300

    feat: adicionar as novas rotas de tarefas

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import taskController from "../controllers/taskController";
-import authMiddleware from "../middlewares/authMiddleware";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 router.post("/", authMiddleware, taskController.addTask);
 router.get("/", authMiddleware, taskController.getTasks);
+router.put("/complete", authMiddleware, taskController.completeTask);
+router.get("/history", authMiddleware, taskController.getTaskHistory);
 
 export default router;
