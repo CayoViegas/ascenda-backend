137a3ab8b640ca9474e4518b83857361162234bd
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2024-11-20 02:14:39 -0300

    feat: implementar método para listar tarefas

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { db } from "../config/database";
 import { tasks } from "../models/task";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { CustomJwtPayload } from "../types/customJwtPayload";
 
 const taskController = {
@@ -32,6 +32,34 @@ const taskController = {
             res.status(201).json({ message: "Task added successfully." });
         } catch (error) {
             console.error("Error adding task: ", error);
+            res.status(500).json({ message: "Internal server error." });
+        }
+    },
+
+    async getTasks(req: Request, res: Response): Promise<void> {
+        const userId = (req.user as CustomJwtPayload)?.id;
+
+        if (!userId) {
+            res.status(401).json({ message: "Unauthorized." });
+            return;
+        }
+
+        try {
+            const today = new Date().toISOString().split("T")[0];
+
+            const userTasks = await db
+                .select()
+                .from(tasks)
+                .where(
+                    and(
+                        eq(tasks.userId, userId),
+                        eq(tasks.date, today)
+                    )
+                );
+
+            res.json(userTasks);
+        } catch (error) {
+            console.error("Error getting tasks: ", error);
             res.status(500).json({ message: "Internal server error." });
         }
     }
